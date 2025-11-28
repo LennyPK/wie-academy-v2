b060594cc3a6d680aaa20920d74c8641f1468ff0
Christopher Jensen <127068230+Kuwisu@users.noreply.github.com>
2025-11-29 01:42:31 +1300

    ✨ feat [BACKEND]: Add member info tables to schema (#1)
    * ✨ feat [BACKEND]: Add member info tables to schema
    * ✨ feat [BACKEND]: Re-add tables mistakenly deleted on rebase
    * 💄 style: Align writing in user table to match longer data type
    * ✨ feat [BACKEND]: Add data values to enumerated tables
    * ♻️ refactor: Change approval status to enum
    * ✨ feat: Add role enum, format schema and fix type errors
    * 👷 ci: Delete package-lock.json from branch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -31,12 +31,77 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ]
 
+const regionData: Prisma.UserCreateInput[] = [
+  { value: "northland-region", label: "Northland" },
+  { value: "auckland-region", label: "Auckland" },
+  { value: "waikato-region", label: "Waikato" },
+  { value: "bay-of-plenty-region", label: "Bay of Plenty" },
+  { value: "gisborne-region", label: "Gisborne" },
+  { value: "hawkes-bay-region", label: "Hawke's Bay" },
+  { value: "taranaki-region", label: "Taranaki" },
+  { value: "manawatu-whanganui-region", label: "Manawatū-Whanganui" },
+  { value: "wellington-region", label: "Wellington" },
+  { value: "tasman-region", label: "Tasman" },
+  { value: "nelson-region", label: "Nelson" },
+  { value: "marlborough-region", label: "Marlborough" },
+  { value: "west-coast-region", label: "West Coast" },
+  { value: "canterbury-region", label: "Canterbury" },
+  { value: "otago-region", label: "Otago" },
+  { value: "southland-region", label: "Southland" },
+  { value: "area-outside-region", label: "Area Outside Region" },
+]
+
+const yearLevelData: Prisma.UserCreateInput[] = [
+  { value: "year-9", label: "Year 9" },
+  { value: "year-10", label: "Year 10" },
+  { value: "year-11", label: "Year 11" },
+  { value: "year-12", label: "Year 12" },
+  { value: "year-13", label: "Year 13" },
+]
+
+const interestData: Prisma.UserCreateInput[] = [
+  { value: "aerospace", label: "Aerospace" },
+  { value: "artificial-intelligence", label: "Artificial Intelligence" },
+  { value: "complex-systems", label: "Complex Systems" },
+  { value: "construction", label: "Construction" },
+  { value: "electronics", label: "Electronics" },
+  { value: "hardware", label: "Hardware" },
+  { value: "health-technology", label: "Health Technology" },
+  { value: "manufacturing", label: "Manufacturing" },
+  { value: "robotics", label: "Robotics" },
+  { value: "sustainability", label: "Sustainability" },
+]
+
 export async function main() {
   for (const user of userData) {
     await prisma.user.upsert({
       where: { email: user.email },
       update: {},
       create: user,
+    })
+  }
+
+  for (const region of regionData) {
+    await prisma.region.upsert({
+      where: { label: region.label },
+      update: {},
+      create: region,
+    })
+  }
+
+  for (const yearLevel of yearLevelData) {
+    await prisma.yearLevel.upsert({
+      where: { label: yearLevel.label },
+      update: {},
+      create: yearLevel,
+    })
+  }
+
+  for (const interest of interestData) {
+    await prisma.interest.upsert({
+      where: { label: interest.label },
+      update: {},
+      create: interest,
     })
   }
 }
