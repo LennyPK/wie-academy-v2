ad2bb4bc0ab16d42e328884c95c7541bb34fdc2f
Lenny <111238403+LennyPK@users.noreply.github.com>
2025-12-10 14:45:59 +1300

    :recycle: refactor [BACKEND]: Organise Prisma files and imports

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,5 +1,7 @@
+export * from "./announcement-categories"
 export * from "./regions"
 export * from "./year-levels"
 
+export { getAnnouncementCategories } from "./announcement-categories"
 export { getRegions } from "./regions"
 export { getYearLevels } from "./year-levels"
