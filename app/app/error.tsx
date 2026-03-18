b4b76f3d04193c2ae4a51a1a66acdbc63092c88b
Lenny <111238403+LennyPK@users.noreply.github.com>
2026-03-19 02:38:31 +1300

    :lipstick: styling [UI]: Add styling to match other error pages

--- app/app/error.tsx
+++ app/app/error.tsx
@@ -1,7 +1,16 @@
 "use client"
 
-// FIXME: Barebones error component
-
+import { Button } from "@/components/ui/button"
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/components/ui/empty"
+import { ROUTES } from "@/lib/constants"
+import { HomeIcon, RotateCw } from "lucide-react"
+import Link from "next/link"
 import { useEffect } from "react"
 
 export default function Error({
@@ -12,21 +21,43 @@ export default function Error({
   reset: () => void
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
+    // TODO: Log the error to an error reporting service
     console.error(error)
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="relative flex min-h-screen w-full items-center justify-center overflow-hidden">
+      <Empty>
+        <EmptyHeader>
+          <EmptyTitle className="mask-b-from-20% mask-b-to-80% text-9xl font-extrabold text-primary">
+            500
+          </EmptyTitle>
+          <EmptyDescription className="-mt-8 text-nowrap text-foreground/80">
+            Something went wrong!
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <div className="flex gap-2">
+            <Button asChild variant="outline">
+              <Link href={ROUTES.DASHBOARD} replace>
+                <HomeIcon className="mr-2 h-4 w-4" />
+                Go Home
+              </Link>
+            </Button>
+
+            <Button
+              variant="default"
+              onClick={
+                // Attempt to recover by trying to re-render the segment
+                () => reset()
+              }
+            >
+              <RotateCw className="mr-2 h-4 w-4" />
+              Try again
+            </Button>
+          </div>
+        </EmptyContent>
+      </Empty>
     </div>
   )
 }
